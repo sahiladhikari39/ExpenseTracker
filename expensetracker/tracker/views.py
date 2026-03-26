66cb62a7dd62701e660a86420adf92dbb7f033b9
Sahil Adhikari <sahiladhikari39@gmail.com>
2026-03-26 17:54:11 +0545

    project finished

--- expensetracker/tracker/views.py
+++ expensetracker/tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import TrackingHistory, CurrentBalance
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -16,7 +17,31 @@ def index(request):
         current_balance.current_balance += float(tracking_history.amount)
         current_balance.save()
 
-        print(f"{description} {amount}")
+        
         return redirect('/')
+    
+    income = 0
+    expense = 0
+    for data in TrackingHistory.objects.all():
+        if data.expense_type == "CREDIT":
+            income += data.amount
+        else:
+            expense += data.amount
 
-    return render(request, "tracker/index.html")
+    current_balance, _ = CurrentBalance.objects.get_or_create(id=1) 
+    context = {"income": income, "expense": expense, "transactions": TrackingHistory.objects.all(), "current_balance": current_balance}
+    return render(request, "tracker/index.html", context)
+
+
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id=id)
+
+    if tracking_history.exists():
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        tracking_history = tracking_history[0]
+
+        current_balance.current_balance = current_balance.current_balance - tracking_history.amount
+        current_balance.save()
+
+    tracking_history.delete()
+    return redirect('/')
